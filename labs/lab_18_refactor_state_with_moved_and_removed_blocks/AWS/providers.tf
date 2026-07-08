267ff61c1641488cfd3474606c2d5789710ddda8
Bryan Krausen <--unset>
2026-07-08 18:09:49 -0400

    adding new refactor lab

--- labs/lab_18_refactor_state_with_moved_and_removed_blocks/AWS/providers.tf
+++ labs/lab_18_refactor_state_with_moved_and_removed_blocks/AWS/providers.tf
@@ -1,13 +1,13 @@
 terraform {
-  required_version = ">= 1.10.0"
+  required_version = ">= 1.12.2"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0"
+      version = "~> 6.0"
     }
   }
 }
 
 provider "aws" {
-  region = var.region
+  region = var.aws_region
 }
